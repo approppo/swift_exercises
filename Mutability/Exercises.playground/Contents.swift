244f98ae5e35ddfdb41cb34d1e965ffa4341c0f5
Roger Kislig <roger.kislig@approppo.ch>
2017-11-09 16:16:18 +0100

    added second day chapters 10-19

---
+++ Mutability/Exercises.playground/Contents.swift
@@ -0,0 +1,20 @@
+struct Person {
+    var name: String
+    var age: Int
+    
+    func speak() -> String {
+        return "Hi! My name is \(name), I am \(age) years old."
+    }
+    
+    mutating func increase() {
+        age += 1
+    }
+}
+
+var peter = Person(name: "Peter", age: 30)
+peter.speak()
+peter.increase()
+
+var john = Person(name: "John", age: 20)
+john.speak()
+john.increase()
