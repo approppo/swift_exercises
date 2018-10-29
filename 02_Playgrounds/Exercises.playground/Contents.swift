3ec19eeb4f1800aff3fcebc5a0601cf4184f90b4
Roger Kislig <roger.kislig@approppo.ch>
2018-10-29 21:30:57 +0100

    renamed 12-14

--- 02_Playgrounds/Exercises.playground/Contents.swift
+++ 02_Playgrounds/Exercises.playground/Contents.swift
@@ -6,7 +6,7 @@ label.frame = CGRect(x: 50, y: 100, width: 250, height: 100)
 label.text = "This is a label!"
 label.textColor = .white
 label.backgroundColor = .blue
-label.font = UIFont.boldSystemFont(ofSize: 30)
+label.font = .boldSystemFont(ofSize: 30)
 
 let controller = UIViewController()
 controller.view.backgroundColor = .lightGray
